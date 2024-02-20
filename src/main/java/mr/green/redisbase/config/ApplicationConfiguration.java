252a6a6c25212c937bcba57fac4389d3897486a7
Василенко Илья <ivvasilen1@mts.ru>
2024-02-20 14:33:33 +0300

    добавлена конфигурация redisTemplate

---
+++ src/main/java/mr/green/redisbase/config/ApplicationConfiguration.java
@@ -0,0 +1,34 @@
+package mr.green.redisbase.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import static io.lettuce.core.ReadFrom.REPLICA_PREFERRED;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public RedisConnectionFactory connectionFactory(@Value("${redis.host}") String host,
+                                                    @Value("${redis.port}") String port) {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(REPLICA_PREFERRED)
+                .build();
+
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(host, Integer.parseInt(port));
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
