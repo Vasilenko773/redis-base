194fe72fa4fb6641e8175dbf2cf3f847900aa30a
Василенко Илья <ivvasilen1@mts.ru>
2024-02-20 22:27:30 +0300

    feature: entity аннотированны @RedisHash

--- src/main/java/mr/green/redisbase/book/Book.java
+++ src/main/java/mr/green/redisbase/book/Book.java
@@ -1,13 +1,17 @@
 package mr.green.redisbase.book;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 import java.util.List;
 
 @Data
+@RedisHash(value = "people", timeToLive = 60L)
 public class Book implements Serializable {
 
+    @Id
     private String id;
     private int pageCount;
     private String thumbnail;
--- src/main/java/mr/green/redisbase/user/User.java
+++ src/main/java/mr/green/redisbase/user/User.java
@@ -1,7 +1,29 @@
 package mr.green.redisbase.user;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
-public record User(UUID id, String password, String name, String email) {
 
+@Data
+@RedisHash
+public class User implements Serializable {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private UUID id;
+    private String password;
+    private String name;
+    private String email;
+    private Set<Role> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
 }
