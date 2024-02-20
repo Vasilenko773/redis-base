558a2a85a4965161994a5f533a72601e21c3e531
Василенко Илья <ivvasilen1@mts.ru>
2024-02-20 15:38:27 +0300

    добавлена конфигурация spring security

--- src/main/java/mr/green/redisbase/config/SecurityConfiguration.java
+++ src/main/java/mr/green/redisbase/config/SecurityConfiguration.java
@@ -1,33 +1,72 @@
 package mr.green.redisbase.config;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 
 @Configuration
 public class SecurityConfiguration {
 
+    /**
+     * @return encoder который кодирует и декодирует пароли
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * @return хранилище пользователей и их ролей
+     */
     @Bean
     public UserDetailsService inMemoryDetailsService(@Value("${security.user.name}") String username,
-                                                     @Value("${security.user.password}") String password) {
+                                                     @Value("${security.user.password}") String password,
+                                                     PasswordEncoder passwordEncoder) {
         InMemoryUserDetailsManager memoryUserDetailsManager = new InMemoryUserDetailsManager();
         memoryUserDetailsManager.createUser(
                 new User(username,
-                        passwordEncoder().encode(password),
+                        passwordEncoder.encode(password),
                         List.of(new SimpleGrantedAuthority("ADMIN"))));
         return memoryUserDetailsManager;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService inMemoryDetailsService,
+                                                         PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(inMemoryDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        return authProvider;
+    }
+
+    /**
+     * @return сконфигурируемый компонент аутентификации и авторизации
+     */
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .httpBasic();
+
+        return http.build();
     }
 }
